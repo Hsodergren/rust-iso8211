f96ea6e6cbf635ab7d45e6022be4c35b84ad47ba
Emil Södergren <EmilSodergren@users.noreply.github.com>
2018-12-05 15:33:18 +0100

    Use regex to parse data types

--- src/data_parser.rs
+++ src/data_parser.rs
@@ -1,12 +1,19 @@
+use regex::Regex;
 use std::io::Read;
 
-#[derive(Debug, PartialEq)]
+lazy_static! {
+    // Maybe a number folowed by A,I,R followed by maybe a parenthesied number
+    // See tests
+    static ref FIELD_REGEX: Regex = Regex::new(r"^(\d+)?([AIR])(\(\d*\))?").unwrap();
+}
+
+#[derive(Debug, PartialEq, Clone)]
 pub(crate) enum ParseData {
     Fixed(ParseType, usize),
     Variable(ParseType),
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub(crate) enum ParseType {
     Integer,
     String,
@@ -22,21 +29,29 @@ pub enum Data {
 
 #[derive(Debug, PartialEq)]
 pub enum E {
+    UnParsable(String),
     UnknownDataType(char),
-}
-
-enum ParseState {
-    Start,
-    Type,
-    Num,
-    Length,
 }
 
 impl ParseData {
     pub(crate) fn new(s: &str) -> Result<(usize, ParseData), E> {
-        let state = ParseState::Start;
-        for c in s.chars() {}
-        Ok((1, ParseData::Variable(ParseType::Integer)))
+        match FIELD_REGEX.captures(s) {
+            Some(cap) => {
+                let num = cap.get(1).map_or(1, |c| c.as_str().parse().unwrap());
+                let typ = match cap.get(2).unwrap().as_str() {
+                    "A" => ParseType::String,
+                    "I" => ParseType::Integer,
+                    "R" => ParseType::Float,
+                    _ => unreachable!(),
+                };
+                let pd = cap.get(3).map_or(ParseData::Variable(typ.clone()), |c| {
+                    let c = &c.as_str();
+                    ParseData::Fixed(typ, c[1..c.len() - 1].parse().unwrap())
+                });
+                Ok((num, pd))
+            }
+            None => Err(E::UnParsable(String::from(s))),
+        }
     }
 
     pub fn parse<R: Read>(rdr: R) -> Result<Data, E> {
@@ -73,6 +88,18 @@ mod tests {
         assert_eq!(
             ParseData::new("1A").unwrap(),
             (1, ParseData::Variable(ParseType::String))
+        );
+        assert_eq!(
+            ParseData::new("2A(3)").unwrap(),
+            (2, ParseData::Fixed(ParseType::String, 3))
+        );
+        assert_eq!(
+            ParseData::new("10I(10)").unwrap(),
+            (10, ParseData::Fixed(ParseType::Integer, 10))
+        );
+        assert_eq!(
+            ParseData::new("1R(5)").unwrap(),
+            (1, ParseData::Fixed(ParseType::Float, 5))
         );
     }
 }
