4a2ee0d8978f13660f2327519e2371d2603f46b0
Emil Södergren <EmilSodergren@users.noreply.github.com>
2019-01-22 13:21:20 +0100

    Reworked the test to get better printouts

--- tests/catalog_tests.rs
+++ tests/catalog_tests.rs
@@ -4,9 +4,11 @@ use std::fs::File;
 type Result<T> = std::result::Result<T, failure::Error>;
 
 fn print_error(err: &failure::Error) {
+    println!("");
     for c in err.iter_chain() {
         println!("{}", c);
     }
+    println!("");
 }
 
 #[test]
@@ -19,10 +21,19 @@ fn test_parse_catalog() {
 }
 
 fn try_main() -> Result<()> {
-    let cf = File::open("tests/CATALOG.031.htm").unwrap();
+    let cf = File::open("tests/CATALOG.031").unwrap();
     let catalog = Catalog::new(cf)?;
-    for record in catalog.take(500) {
-        println!("{:?}", record?);
+    let mut last_record = None;
+    for record in catalog {
+        match record {
+            Ok(r) => last_record = Some(r),
+            Err(err) => {
+                println!("Last successful parse was:");
+                println!("{:?}", last_record);
+                print_error(&err.into());
+                panic!();
+            }
+        }
     }
     Ok(())
 }
